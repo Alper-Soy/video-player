e2b6e836ecc879104ee4c7b8948c51dde5156218
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-14 20:18:42 +0300

    Youtube Player

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+import youtube from '../apis/youtube';
+import SearchBar from './SearchBar';
+import VideoDetail from './VideoDetail';
+import VideoList from './VideoList';
+
+const KEY = 'AIzaSyAcvV1plGj_9uQgSd-vcQRidCkzlPFdfdk';
+
+class App extends Component {
+  state = { videos: [], selectedVideo: null };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        maxResults: 5,
+        type: 'video',
+        key: KEY,
+      },
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className='ui container' style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onTermSubmit} />
+        <div className='ui grid'>
+          <div className='ui row'>
+            <div className='eleven wide column'>
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className='five wide column'>
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
